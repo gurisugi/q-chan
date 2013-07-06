60608c97768d22f4ddcc1a7490043011b92decaa
gurisugi <gurisugi@gmail.com>
2013-07-07 01:03:35 +0900

    fix
    fix botname
    read ssl setting from config
    separate cmd

--- q-chan.pl
+++ q-chan.pl
@@ -7,9 +7,10 @@ use AnySan;
 use AnySan::Provider::IRC;
 use Config::PL;
 use Redis;
+use List::MoreUtils qw/any/;
 
 my $conf         = config_do 'config.pl';
-my $bot_nickname = 'q-chan';
+my $bot_nickname = 'qchan';
 my $irc          = irc
     $conf->{host},
     port          => $conf->{port},
@@ -22,9 +23,8 @@ my $irc          = irc
         sleep 10;
         shift->connect;
     },
-    enable_ssl => 1,
+    enable_ssl => $conf->{enable_ssl},
 ;
-$irc->join_channel('#oppai');
 
 my $redis = Redis->new(
     encoding  => undef,
@@ -32,7 +32,6 @@ my $redis = Redis->new(
     every     => 200,
 );
 
-$redis->flushall;
 
 AnySan->register_listener(
     oppai => {
@@ -40,77 +39,23 @@ AnySan->register_listener(
             my $r = shift;
             my $from_nick = $r->from_nickname;
             my $message = ( $r->message );
-            return unless $message =~ /^q_chan:/;
+            return unless $message =~ /^$bot_nickname/;
+
+            my $m = _parse_message( $message, $bot_nickname );
+            return unless $m;
 
             my $reply;
-            my $m = _parse_message( $message );
-                return unless $m;
-            }
+            my ( $method, @args ) = split ' ', $m;
 
-            my ($q_nick, $method, $target_nick) = split ' ', $m;
-            $q_nick ||= '';
-            $method ||= '';
-            $target_nick ||= '';
-
-            if ($method =~ /^show$/) {
-                my @res = $redis->lrange( $q_nick, 0, -1 );
-                my $q = join ', ', @res;
-                $reply = $q ? "$from_nick: $q_nick => [ $q ]" : "No one in $q_nick queue."
-            }
-            elsif ( $method =~ /^add$/ ) {
-                unless ( $target_nick ) {
-                    $redis->rpush( $q_nick, $from_nick );
-                    $reply = "$from_nick: ok. $from_nick add to $q_nick queue.";
-                }
-                else {
-                    $redis->rpush( $q_nick, $target_nick );
-                    $reply = "$from_nick: ok. $target_nick add to $q_nick queue.";
-                }
-            }
-            elsif ( $method =~ /^done$/ ) {
-                unless ( $target_nick ) {
-                    my @lis = $redis->lrange( $q_nick, 0, 0 );
-                    $target_nick = $lis[0];
-                    unless ( $target_nick ) {
-                        $r->send_reply( "No one in $q_nick queue." );
-                        return;
-                    }
-                }
-                if ( $redis->lrem( $q_nick, 1, $target_nick ) ) {
-                    my @next_nick = $redis->lrange( $q_nick, 0, 0 );
-                    my $next_message = $next_nick[0] ? "And next one is $next_nick[0]." : "No one in $q_nick queue.";
-                    $reply = "$from_nick: OK. Delete $target_nick from $q_nick queue. $next_message";
-                }
-                else {
-                    $reply = "$from_nick: $target_nick not found in $q_nick queue.";
-                }
-            }
-            elsif ( $q_nick =~ /^all$/ ) {
-                my @keys = $redis->keys( '*' );
-                unless ( scalar @keys ) {
-                    $r->send_reply( "No one in any queue." );
-                }
-                else {
-                    for my $key ( @keys ) {
-                        my @lis = $redis->lrange( $key, 0, -1 );
-                        my $value = join ', ', @lis;
-                        $reply = "$key => [ $value ]";
-                        $r->send_reply( $reply );
-                    }
-                }
-                return;
-            }
-            elsif ( $q_nick =~ /^help$/ ) {
-                $r->send_reply( "＿ﾉ乙(､ﾝ､)つ https://github.com/gurisugi/q-chan/blob/master/README.md" );
+            if ( _is_valid_method( $method ) ) {
+                $reply = eval 'cmd_'.$method.'( $redis, @args )';
+                $r->send_reply( $reply );
                 return;
             }
             else {
-                $reply = "$m ＿ﾉ乙(､ﾝ､)?";
+                $r->send_reply( '＿ﾉ乙(､ﾝ､)つ'."'".$method."'".'？' );
+                return;
             }
-
-            return unless $reply;
-
-            $r->send_reply( $reply );
         }
     }
 );
@@ -118,9 +63,10 @@ AnySan->register_listener(
 AnySan->run;
 
 
+
 sub _parse_message {
-    my $message = shift;
-    $message =~ /(^q_chan:)(.+)/;
+    my ( $message, $bot_nickname ) = @_;
+    $message =~ /(^$bot_nickname:?)([^:]+)/;
     my $q_chan = $1
         or return undef;
     my $he_says = $2
@@ -128,4 +74,64 @@ sub _parse_message {
     $he_says =~ s/^\s//;
 
     return $he_says ? $he_says : undef;
+}
+
+sub _is_valid_method {
+    my ( $method ) = @_;
+    my @methods = (
+        'show',
+        'add',
+        'done',
+        'all',
+        'help',
+    );
+
+    return ( any { $method =~ /^$_$/ } @methods ) ? 1 : 0;
+}
+
+
+
+##### cmds ####
+
+sub cmd_all {
+    my $redis = shift;
+    my @keys = $redis->keys('*');
+    my $reply = scalar @keys
+        ? join ', ', map { $_.' => ['.join( ', ', $redis->lrange($_, 0, -1) ).']' } @keys
+        : 'Nobody in any queue.';
+    return $reply;
+}
+
+sub cmd_help {
+    my $reply =  "＿ﾉ乙(､ﾝ､)つ https://github.com/gurisugi/q-chan/blob/master/README.md";
+    return $reply;
+}
+
+sub cmd_show {
+    my $redis = shift;
+    my ( $key_user ) = @_;
+
+    my @queue_mem = $redis->lrange( $key_user, 0, -1 );
+    my $reply = scalar @queue_mem
+        ? $key_user.' => ['.join( ', ', @queue_mem ).']'
+        : 'Nobody in '.$key_user.' queue.';
+    return $reply;
+}
+
+sub cmd_done {
+    my $redis = shift;
+    my ( $key_user, $target_user ) = @_;
+
+    my $res = $redis->lpop( $key_user );
+    my $reply = $res ? 'done success' : 'done failed';
+    return $reply;
+}
+
+sub cmd_add {
+    my $redis = shift;
+    my ( $key_user, $target_user ) = @_;
+
+    my $res = $redis->rpush( $key_user, $target_user );
+    my $reply = $res ? 'add success.' : 'add failed.';
+    return $reply;
 }
